9b6aa1e2016f59804911f3e1b233ba9a7accc44c
faizahkhan <54927338+faizahkhan@users.noreply.github.com>
2021-03-12 20:32:29 -0500

    final

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,22 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User extends Tweet {
+
+    public String name;
+    public String screenName;
+    public String profileImageURL;
+
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+        User user = new User ();
+        user.name = jsonObject.getString("name");
+        user.screenName = jsonObject.getString("screen_name");
+        user.profileImageURL = jsonObject.getString("profile_image_url_https");
+        return user;
+    }
+}
